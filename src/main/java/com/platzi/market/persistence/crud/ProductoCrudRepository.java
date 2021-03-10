15a3a2036c576dc1e65a23153aee09dd2878f707
JonatSrG <JonatSrG>
2021-03-09 21:03:09 -0600

    metodos query

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,12 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoria(int idCategoria);
 }
