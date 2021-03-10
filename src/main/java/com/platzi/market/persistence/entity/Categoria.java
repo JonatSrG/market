5ed0f2d36c3b59786922c70672f4f921d5d9a23d
JonatSrG <JonatSrG>
2021-03-09 20:22:57 -0600

    relacion entre mapeos y clases

--- src/main/java/com/platzi/market/persistence/entity/Categoria.java
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "categorias")
-public class categoria {
+public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/platzi/market/persistence/entity/Cliente.java
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -7,7 +7,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "clientes")
-public class cliente {
+public class Cliente {
 
     @Id
     private String id;
